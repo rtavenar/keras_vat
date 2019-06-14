d36c01dd424453f44719719fdbb3ea7655025cc4
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2019-06-14 16:05:58 +0200

    Semisupervised model OK, to be tested

--- mnist.py
+++ mnist.py
@@ -2,6 +2,7 @@ from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
 from keras.datasets import mnist
+import numpy
 
 from model import VATModel, SemiSupervisedVATModel
 
@@ -34,22 +35,36 @@ X_test /= 255.
 y_train = to_categorical(y_train, n_classes)
 y_test = to_categorical(y_test, n_classes)
 
-# Fully supervised training: do not use VATModel
+# # Fully supervised training: do not use VATModel
 model.compile(optimizer="adam", loss="categorical_crossentropy",
               metrics=["accuracy"])
-model.fit(X_train, y_train, batch_size=128, epochs=3, verbose=2)
-
-# Fully unsupervised training: use VATModel without any other loss function
-model2 = VATModel(inputs=model.inputs, outputs=model.outputs)
-model2.compile(optimizer="adam", loss=[None] * len(model2.outputs),
-               metrics=["accuracy"])
-model2.fit(X_train, None, batch_size=128, epochs=3, verbose=2)
+# model.fit(X_train, y_train, batch_size=128, epochs=3, verbose=2)
+#
+# # Fully unsupervised training: use VATModel without any other loss function
+# model2 = VATModel(model=model, input_shape=(28, 28, 1))
+# model2.compile(optimizer="adam", loss=[None] * len(model2.outputs),
+#                metrics=["accuracy"])
+# model2.fit(X_train, None, batch_size=128, epochs=3, verbose=2)
 
 # Semi-supervised training: define a model with two inputs/outputs/losses, one
 # for supervised data, one for unsupervised data
-model3 = SemiSupervisedVATModel(inputs=model.inputs, outputs=model.outputs)
-model3.compile(optimizer="adam", loss=["categorical_crossentropy", None],
+indices_supervised = numpy.random.randint(low=0, high=X_train.shape[0],
+                                          size=X_train.shape[0] // 500)
+indices_unsupervised = numpy.isin(numpy.arange(X_train.shape[0]),
+                                  indices_supervised,
+                                  invert=True)
+X_train_sup = X_train[indices_supervised]
+y_train_sup = y_train[indices_supervised]
+X_train_unsup = X_train[indices_unsupervised]
+
+model3 = SemiSupervisedVATModel(model=model)
+model3.compile(optimizer="adam", loss="categorical_crossentropy",
                metrics=["accuracy"])
 model3.fit([X_train_sup, X_train_unsup],
            [y_train_sup, None],
-           batch_size=128, epochs=3, verbose=2)
+           batch_size=128, epochs=10)
+print(model3.evaluate(X_test, y_test))
+# model.fit(X_train_sup, y_train_sup, batch_size=128,
+#           epochs=10 * len(X_train_unsup) // len(X_train_sup),  # trick to make number of batch updates similar to that of the semi-supervised model
+#           verbose=2)
+# print(model.evaluate(X_test, y_test))
--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 from functools import reduce
 from keras.utils.generic_utils import to_list
 from keras.engine.training import Model
+from keras.layers import Input
 import keras.backend as K
+import numpy
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
@@ -9,8 +11,11 @@ __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 class VATModel(Model):
     # Widely inspired from
     # https://gist.github.com/mokemokechicken/2658d036c717a3ed07064aa79a59c82d
-    def __init__(self, inputs, outputs, xi=1e-3, eps=1.0, ip=1):
-        super(VATModel, self).__init__(inputs=inputs, outputs=outputs)
+    def __init__(self, model, input_shape=(28, 28, 1), xi=1e-3, eps=1.0, ip=1):  # TODO: input_shape
+        vat_inputs = [Input(shape=input_shape) for i in model.inputs]  # TODO
+        vat_outputs = model(vat_inputs)
+        super(VATModel, self).__init__(inputs=vat_inputs,
+                                       outputs=vat_outputs)
         self._vat_loss = None
         self.setup_vat_loss(eps=eps, xi=xi, ip=ip)
 
@@ -54,7 +59,73 @@ class VATModel(Model):
 
 
 class SemiSupervisedVATModel(Model):
-    def __init__(self, inputs, outputs):
-        super(SemiSupervisedVATModel, self).__init__(inputs=inputs,
-                                                     outputs=outputs)
-        # TODO
+    def __init__(self, model, input_shape=(28, 28, 1), xi=1e-3, eps=1.0, ip=1):
+        supervised_model = model
+        unsupervised_model = VATModel(model, input_shape=input_shape,
+                                      xi=xi, eps=eps, ip=ip)
+        all_inputs = supervised_model.inputs + unsupervised_model.inputs
+        all_outputs = supervised_model.outputs + unsupervised_model.outputs
+        super(SemiSupervisedVATModel, self).__init__(inputs=all_inputs,
+                                                     outputs=all_outputs)
+
+        self.supervised_model = supervised_model
+        self.unsupervised_model = unsupervised_model
+
+    def compile(self, optimizer, loss, metrics=[]):
+        self.supervised_model.compile(optimizer=optimizer,
+                                      loss=loss,
+                                      metrics=metrics)
+        self.unsupervised_model.compile(optimizer=optimizer, loss=[None])
+
+    @property
+    def metrics_names(self):
+        return ["sup_" + m for m in self.supervised_model.metrics_names] + \
+               ["unsup_" + m for m in self.unsupervised_model.metrics_names]
+
+    def fit(self, x=None, y=None, batch_size=32, epochs=1):
+        # One item for supervised, one for unsupervised
+        assert len(x) == len(y) == 2
+        n_sup = x[0].shape[0]
+        n_unsup = x[1].shape[0]
+        n = max([n_sup, n_unsup])
+        for e in range(epochs):
+            n_batches = n // batch_size
+            loss_sup = numpy.array([0.] * len(self.supervised_model.metrics_names))
+            loss_unsup = numpy.array([0.] * len(self.supervised_model.metrics_names))
+            for b in range(n_batches):
+                indices_sup = numpy.random.randint(low=0, high=n_sup,
+                                                   size=batch_size)
+                indices_unsup = numpy.random.randint(low=0, high=n_unsup,
+                                                     size=batch_size)
+                x_sup_batch = x[0][indices_sup]
+                y_sup_batch = y[0][indices_sup]
+                x_unsup_batch = x[1][indices_unsup]
+                loss_sup += self.supervised_model.train_on_batch(x_sup_batch,
+                                                                 y_sup_batch)
+                loss_unsup += self.unsupervised_model.train_on_batch(x_unsup_batch,
+                                                                     None)
+            loss_sup /= n_batches
+            loss_unsup /= n_batches
+            loss = numpy.stack((loss_sup, loss_unsup)).reshape((-1, ))
+            s = "epoch {}/{}: ".format(e + 1, epochs)
+            for i, name in enumerate(self.metrics_names):
+                s += "{}: {} ".format(name, loss[i])
+            print(s)
+        return [-1., -1.]  # TODO
+
+    def predict(self, x,
+                batch_size=None,
+                verbose=0,
+                steps=None):
+        return self.supervised_model.predict(x=x, batch_size=batch_size,
+                                             verbose=verbose, steps=steps)
+
+    def evaluate(self, x=None, y=None,
+                 batch_size=None,
+                 verbose=1,
+                 sample_weight=None,
+                 steps=None):
+        return self.supervised_model.evaluate(x=x, y=y,
+                                              sample_weight=sample_weight,
+                                              batch_size=batch_size,
+                                              verbose=verbose, steps=steps)
